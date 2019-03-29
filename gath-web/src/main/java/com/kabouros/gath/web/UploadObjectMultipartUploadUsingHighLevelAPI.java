ff625148d3c27060708e0b8fa6948804d0266c3f
jiangcihuo <chiang-fire@outlook.com>
2019-03-29 18:01:04 +0800

    feat: add TransferManager

---
+++ gath-web/src/main/java/com/kabouros/gath/web/UploadObjectMultipartUploadUsingHighLevelAPI.java
@@ -0,0 +1,35 @@
+package com.kabouros.gath.web;
+
+import java.io.File;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
+ 
+public class UploadObjectMultipartUploadUsingHighLevelAPI {
+ 
+    public static void main(String[] args) throws Exception {
+        String existingBucketName = "*** Provide existing bucket name ***";
+        String keyName            = "*** Provide object key ***";
+        String filePath           = "*** Path to and name of the file to upload ***"; 
+         
+        TransferManager tm = TransferManagerBuilder.standard().withS3Client(AmazonS3ClientBuilder.standard().withCredentials(new ProfileCredentialsProvider()).build()).build();
+        //TransferManager tm = new TransferManager(new ProfileCredentialsProvider());       
+        System.out.println("Hello");
+        // TransferManager processes all transfers asynchronously,
+        // so this call will return immediately.
+        Upload upload = tm.upload(existingBucketName, keyName, new File(filePath));
+        System.out.println("Hello2");
+        try {
+            // Or you can block and wait for the upload to finish
+            upload.waitForCompletion();
+            System.out.println("Upload complete.");
+        } catch (AmazonClientException amazonClientException) {
+            System.out.println("Unable to upload file, upload was aborted.");
+            amazonClientException.printStackTrace();
+        }
+    }
+}
